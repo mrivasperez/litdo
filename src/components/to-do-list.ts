ff440607ba0853378f5eb6899faaa2764a07f82d
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-12-05 10:24:46 -0800

    style: style to-do-list component

--- src/components/to-do-list.ts
+++ src/components/to-do-list.ts
@@ -1,4 +1,4 @@
-import { LitElement, html } from "lit";
+import { LitElement, css, html } from "lit";
 import { customElement, property } from "lit/decorators.js";
 
 @customElement("to-do-list")
@@ -6,7 +6,49 @@ export class ToDoList extends LitElement {
   @property({ type: Array }) tasks: string[] = ["Brush teeth", "Wash dishes"];
   @property({ type: String }) newTask = "";
 
-  // TODO styles
+  static styles = css`
+    .container {
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+      padding: 1rem;
+    }
+
+    input {
+      padding: 0.5rem;
+      border: 1px solid #ccc;
+      border-radius: 4px;
+    }
+
+    button {
+      padding: 0.5rem 1rem;
+      background-color: #334eff;
+      color: white;
+      border: none;
+      border-radius: 4px;
+      cursor: pointer;
+    }
+
+    ul {
+      list-style: none;
+      padding: 0;
+      border: 1px solid gainsboro;
+      border-radius: 4px;
+    }
+
+    li {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 0.5rem;
+      border-bottom: 1px solid gainsboro;
+    }
+
+    ul li:last-child {
+      border: none;
+    }
+  `;
+
   // TODO addTask
   // TODO deleteTask
 
